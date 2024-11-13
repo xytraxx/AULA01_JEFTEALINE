5c6e406b9196d3c9a84d590487081025f9360725
XytraX <alineruizdias@gmail.com>
2024-11-13 08:15:48 -0300

    Add files via upload

---
+++ AULA01_JS_JEFTE_ALINE/script.js
@@ -0,0 +1,14 @@
+
+//alert("Testando o script!");
+
+var nome = prompt("Qual seu nome?");
+
+var sobrenome = prompt("Qual seu sobrenome?");
+
+console.log("Olá "+nome+" "+sobrenome);
+
+//saída do DOM 
+document.write("<h1>Bem vindo "+nome+" "+sobrenome+" </h1>");
+document.write("Steve através de javascript")
+
+document.write ('<img src="https://sujeitoprogramador.com/steve.png" alt="foto do Steve" />')
